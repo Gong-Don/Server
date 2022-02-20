c81b3ff281430917b962780aa682f02ce98d29ec
choi-jaewon <sodlapdlf13@gmail.com>
2022-02-21 00:50:54 +0900

    #11 refactoring: remove user phone number

--- src/main/java/com/example/gongdon/user/dto/SignupRequest.java
+++ src/main/java/com/example/gongdon/user/dto/SignupRequest.java
@@ -7,12 +7,10 @@ public class SignupRequest {
     private String name;
     private String email;
     private String password;
-    private String phoneNumber;
 
-    public SignupRequest(String name, String email, String password, String phoneNumber) {
+    public SignupRequest(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.phoneNumber = phoneNumber;
     }
 }
