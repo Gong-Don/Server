f434f0b435e882149c39eacfba144f107a9f7242
bsu <bsu0541@naver.com>
2022-03-28 22:33:00 +0900

    #62: refactor interceptor, tokenservice

--- src/main/java/com/example/gongdon/interceptor/Interceptor.java
+++ src/main/java/com/example/gongdon/interceptor/Interceptor.java
@@ -14,13 +14,13 @@ public class Interceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object obj) throws Exception {
-        log.info("url : {}", request.getRequestURI());
+        log.info("method : {}, url : {}", request.getMethod(), request.getRequestURI());
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object obj, ModelAndView mav) throws Exception {
-        log.info("response status: {}", response.getStatus());
+        log.info("response status : {}", response.getStatus());
     }
 
     @Override
